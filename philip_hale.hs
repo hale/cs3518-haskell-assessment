2ddf8d0a367d97bcffdf58408b1d1ddc4192dff0
Philip Hale <p.hale.09@aberdeen.ac.uk>
2013-04-18 17:13:10 +0100

    completed questions 3 and 4

--- philip_hale.hs
+++ philip_hale.hs
@@ -46,4 +46,33 @@ getelt x y
   | x > length y || x < 1 = error "Index out of bounds"
   | otherwise = last ( take x y )
 
+{-
+ - 3. [15 points] Write a function setelt which, given an integer n, a list (of
+ -    items of any type) and some individual of this type, returns the same list
+ -    but with the nth element (with the first element being numbered as 1)
+ -    replaced by the one provided. E.g.
+ -
+ -    Main> setelt 2 [3,4,5,6] 10
+ -    [3,10,5,6]
+ -}
+setelt :: Int -> [x] -> x -> [x]
+setelt n list newElem
+  | n > length list || n < 1 = error "Index out of bounds"
+  | otherwise = beginning ++ newElem : end
+  where (beginning,_:end) = splitAt (n-1) list
+
+{-
+ - 4. [15 points] Write a function combineWithEach which takes as arguments an
+ -    integer x and a list of integers. It returns a similar list, but where 
+ -    each element y has been replaced by (x+y)*(x-y), e.g.
+ -
+ -    Main> combineWithEach 2 [3,5,7]
+ -    [-5,-21,-45]
+ -}
+combineWithEach :: Int -> [Int] -> [Int]
+combineWithEach x [] = []
+combineWithEach n (x:xs) =
+  f x  : combineWithEach n xs
+  where f x = (n+x)*(n-x)
+
 
