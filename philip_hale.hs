1b862aca48483ab74da20b4fd43baa709e888f20
Philip Hale <p.hale.09@aberdeen.ac.uk>
2013-04-19 02:39:21 +0100

    improved documentation

--- philip_hale.hs
+++ philip_hale.hs
@@ -1,9 +1,9 @@
 import Data.List
 {-
- -Title: CS3518 Haskell Assignment 2012-13
- -Due: 19 April 2013, 12 noon
+ -Title:  CS3518 Haskell Assignment 2012-13
+ -Due:    19 April 2013, 12 noon
  -Author: Philip Hale
- -ID: 50907446
+ -ID:     50907446
  -}
 
 {- EXERCISE 1: LIST PROCESSING -}
@@ -42,6 +42,12 @@ member x (y:ys)
  -
  -    Main> getelt 3 [1,2,3,4,5]
  -    3
+ -
+ -    Main> getelt (negate 2) [1,2,3]
+ -    Exception: Index out of bounds
+ -
+ -    Main> getelt 9 []
+ -    Exception: List is empty.
  -}
 getelt :: Int -> [x] -> x
 getelt x [] = error "List is empty"
@@ -57,6 +63,18 @@ getelt x y
  -
  -    Main> setelt 2 [3,4,5,6] 10
  -    [3,10,5,6]
+ -
+ -    Main> setelt 5 [1,2,3,4]
+ -    Exception: Index out of bounds.
+ -
+ -    Main> setelt 9 [1..10] (negate 9)
+ -    [1,2,3,4,5,6,7,8,-9,10]
+ -
+ -    This function first splits the list at the point where the element is to
+ -    be changed.  The head of the second section of the list is discsrded -
+ -    this is the element to be replaced - and a new list is created consisting
+ -    of the first section, the new element, and the rest of the elements.
+ -    (n-1) is used to get 1-indexing.
  -}
 setelt :: Int -> [x] -> x -> [x]
 setelt n list newElem
@@ -71,6 +89,16 @@ setelt n list newElem
  -
  -    Main> combineWithEach 2 [3,5,7]
  -    [-5,-21,-45]
+ -
+ -    Main> combineWithEach 0 [0,1,2,4]
+ -    [0,-1,-4,-16]
+ -
+ -    Main> combineWithEach (negate 2) [3,5,7]
+ -    [-5,-21,-45]
+ -
+ -    This function recirsively iterates through the list, applying the function
+ -    to each element and constructing a new processed list.
+ -
  -}
 combineWithEach :: Int -> [Int] -> [Int]
 combineWithEach x [] = []
@@ -92,10 +120,11 @@ combineWithEach n (x:xs) =
  -    Main> once [negate 2, 3, -2] (negate 2)
  -
  -    This function first checks that the element occurs in the list.  If it
- -    is, the element is removed from the list and the check is performed again.
- -    If it's still in the list, it's a duplicate element.  If not, the
- -    removed element must be unique in the list. It does not work on infinite
- -    lists.
+ -    does, the element is removed from the list and the check is performed
+ -    again. If it's still in the list, it's a duplicate element.  If not, the
+ -    removed element must be unique in the list.
+ -
+ -    Limitation: It does not work on infinite lists.
  -}
 once :: [Int] -> Int -> Bool
 once list n
@@ -103,9 +132,8 @@ once list n
   | n `notElem` list = False
   | otherwise = n `notElem` (delete n list)
 
-{- EXERCISE 2: NOUGHTS AND CROSSES -}
-
-{-
+{- EXERCISE 2: NOUGHTS AND CROSSES
+ -
  - For this exercise, we will represent the state of the board of a noughts and
  - crosses game using lists of lists. We will allow the board to be of any size
  - (and there is no need for you to guarantee that all rows and columns have the
@@ -120,12 +148,15 @@ once list n
   -    suggest that you use an algebraic type ("Data = "), and that you use
   -    "Deriving" to make sure that the marks in a list can be printed (i.e.
   -    "shown"). (This will become relevant in part 2 of this question.)
+  -
+  -    This implementation uses a Type synonym for the Board, since it is
+  -    simply a list of lists of Marks. More complex functionality may benefit
+  -    from switching to a custom algebraic data type for the board.  The
+  -    allowable marks on the board are limited to an X, O or Free which means
+  -    the square is available.
   -}
 data Mark = X | O | Free deriving (Show)
-{-data Board = Board [[Mark]] deriving (Show)-}
 type Board = [[Mark]]
-
-
 
  {- 2. [25 points] Write a function move which, given any board (represented as
   -    above), the number of a row, the number of a column and a mark, returns a
@@ -134,11 +165,20 @@ type Board = [[Mark]]
   -    numbers. Test your function by applying it to the board that you defined
   -    in Part 1 of this question. Do not use the Haskell built-in function !!
   -
+  -    Main> move [[X]] 1 1 O
+  -    [[O]]
+  -
   -    Main> let b = [[X,X,X],[O,O,O],[Free,Free,Free]]
   -    Main> move b 2 2 Free
   -    [[X,X,X],[O,Free,O],[Free,Free,Free]]
+  -
+  -    Main> move [[],[Free]] 1 1 O
+  -    Exception: Index out of bounds.
+  -
+  -    Limitations: Incorrect indexing is handled by the lower level getelem
+  -    and setelem functions, which throw exceptions when an invalid index is
+  -    specified.
   -}
-
 move :: Board -> Int -> Int -> Mark -> Board
 move board x y mark
   | x < 1 || y < 1 = error "Index out of bounds."
